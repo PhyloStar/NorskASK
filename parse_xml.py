15e467bf0608243356c261eb6ceed0a1f6c8bdae
Stig Johan Berggren <stigjb@student.matnat.uio.no>
2018-09-12 21:01:35 +0200

    fix script

--- parse_xml.py
+++ parse_xml.py
@@ -1,15 +1,29 @@
-import xml.etree.ElementTree as ET
+import argparse
 import pathlib
 import re
+import xml.etree.ElementTree as ET
 
 import pandas as pd
 
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', '-o', action='store_true',
+                        help='Output text files') 
+    return parser.parse_args()
+
 def main():
+    args = parse_args()
+
     data_dir = pathlib.Path('ASK')
+    assert data_dir.is_dir()
 
     input_dir = data_dir / 'xml'
+    assert input_dir.is_dir()
     output_dir = data_dir / 'txt'
+
+    if not output_dir.is_dir():
+        output_dir.mkdir()
 
     metadata_dict = {
         "lang": [],
@@ -27,14 +41,18 @@ def main():
 
         language_node = metadata.find("./p[@n='language']")
         cefr_score_node = metadata.find("./p[@n='CEFRscore']")
-        language = language_node.text if language_node else 'N/A'
-        cefr_score = cefr_score_node.text if cefr_score_node else 'N/A'
+        language = language_node.text if language_node is not None else 'N/A'
+        cefr_score = cefr_score_node.text if cefr_score_node is not None else 'N/A'
 
-        metadata_dict['lang'].append(language)
-        metadata_dict['cefr'].append(cefr_score)
+        if language != 'N/A' and cefr_score != 'N/A':
+            metadata_dict['lang'].append(language)
+            metadata_dict['cefr'].append(cefr_score)
 
         text = root.find('text')
         assert text, "Missing text in file " + str(input_file)
+
+        if not args.output: 
+            continue
 
         with open(output_file, 'w') as outfile:
             outfile.write('# language: %s\n' % language)
@@ -53,7 +71,7 @@ def main():
     metadata_df.to_csv('metadata.csv')
     cefr_by_lang = metadata_df.groupby(['lang', 'cefr']).size().unstack()
 
-    print(cefr_by_lang)
+    print(cefr_by_lang.fillna(0))
     cefr_by_lang.plot.bar()
 
 
