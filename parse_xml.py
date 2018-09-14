23b33539d6b5aa7a66a31b07e2815d3d2237b00d
Stig Johan Berggren <stigjb@gmail.com>
2018-09-14 12:24:43 +0200

    expand metadata analysis

--- parse_xml.py
+++ parse_xml.py
@@ -28,13 +28,22 @@ def main():
 
     metadata_dict = {
         "lang": [],
-        "cefr": []
+        "cefr": [],
+        "testlevel": [],
+        "age": [],
+        "gender": [],
+        "topic": []
     }
 
     for input_file in input_dir.iterdir():
-        output_file = str(output_dir / input_file.stem) + '.txt'
+        if input_file.suffix != '.xml':
+            continue
 
-        tree = ET.parse(input_file.open())
+        try:
+            tree = ET.parse(input_file.open())
+        except Exception:
+            print(input_file)
+            raise
         root = tree.getroot()
 
         metadata = root.find('./teiHeader/profileDesc/particDesc/person')
@@ -42,12 +51,28 @@ def main():
 
         language_node = metadata.find("./p[@n='language']")
         cefr_score_node = metadata.find("./p[@n='CEFRscore']")
+        testlevel_node = metadata.find("./p[@n='testlevel']")
+        age_node = metadata.find("./p[@n='age']")
+        gender_node = metadata.find("./p[@n='gender']")
+        topic_node = metadata.find("./p[@n='tema']")
+
         language = language_node.text if language_node is not None else 'N/A'
         cefr_score = cefr_score_node.text if cefr_score_node is not None else 'N/A'
+        testlevel = testlevel_node.text if testlevel_node is not None else 'N/A'
+        try:
+            age = int(age_node.text) if age_node is not None else -1
+        except ValueError:
+            age = -1
+        gender = gender_node.text if gender_node is not None else 'N/A'
+        topic = topic_node.text if topic_node is not None else 'N/A'
 
         if language != 'N/A' and cefr_score != 'N/A':
             metadata_dict['lang'].append(language)
             metadata_dict['cefr'].append(cefr_score)
+            metadata_dict['testlevel'].append(testlevel)
+            metadata_dict['age'].append(age)
+            metadata_dict['gender'].append(gender)
+            metadata_dict['topic'].append(topic)
 
         text = root.find('text')
         assert text, "Missing text in file " + str(input_file)
@@ -55,6 +80,7 @@ def main():
         if not args.output:
             continue
 
+        output_file = str(output_dir / input_file.stem) + '.txt'
         with open(output_file, 'w') as outfile:
             outfile.write('# language: %s\n' % language)
             outfile.write('# CEFRscore: %s\n' % cefr_score)
@@ -70,7 +96,8 @@ def main():
 
     metadata_df = pd.DataFrame(metadata_dict)
     metadata_df.to_csv('metadata.csv')
-    cefr_by_lang = metadata_df.groupby(['lang', 'cefr']).size().unstack()
+    cefr_by_lang = (metadata_df.groupby(['lang', 'cefr']).size().unstack()
+                               .fillna(0).astype(int))
 
     print(cefr_by_lang.fillna(0))
     cefr_by_lang.plot.bar()
