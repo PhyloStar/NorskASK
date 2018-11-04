34b3b62bedb424c85298dd020d40835aba06ea29
Stig Johan Berggren <stigjb@student.matnat.uio.no>
2018-11-04 19:36:13 +0100

    visualize with t-SNE

--- visualize_embeddings.py
+++ visualize_embeddings.py
@@ -1,15 +1,21 @@
 import logging
 import sys
+from collections import namedtuple
 from pathlib import Path
 from typing import TextIO, Iterable
 
-from gensim.models.wrappers.fasttext import FastTextKeyedVectors
+from gensim.models.fasttext import FastTextKeyedVectors
+from gensim.models import FastText
+from sklearn.manifold import TSNE
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from utils import load_fasttext_embeddings
 
 logging.basicConfig(level=logging.INFO)
+
+MockKeyedVectors = namedtuple('MockKeyedVectors', ['vector_size'])
 
 
 def fingerprint(wv: FastTextKeyedVectors, document: Iterable[str]) -> np.ndarray:
@@ -26,10 +32,15 @@ def fingerprint(wv: FastTextKeyedVectors, document: Iterable[str]) -> np.ndarray
     Returns:
         A float array with the same shape as the embeddings in wv
     """
+
     cbow = np.zeros(wv.vector_size, dtype=float)
     token_count = 0
     for token in document:
-        cbow += wv.word_vector(token)
+        try:
+            cbow += wv.word_vec(token)
+        except KeyError:
+            continue
+        # cbow += np.random.randn(wv.vector_size)
         token_count += 1
     cbow /= token_count
     return cbow
@@ -55,18 +66,31 @@ def main():
     meta = pd.read_csv('metadata.csv')
 
     wv = load_fasttext_embeddings(sys.argv[1])
+    # wv = MockKeyedVectors(vector_size=100)
 
     labels = []
     fingerprints = []
+    print('Computing fingerprints of all documents ...')
     for input_file in txt_folder.iterdir():
-        label = meta[meta.filename == input_file.stem].cefr[0]
+        metadata_row = meta[meta.filename == input_file.stem].iloc[0]
+        if metadata_row.cefr is np.nan:
+            continue
+        label = metadata_row.cefr
         labels.append(label)
-        with open(input_file) as f:
-            fingerprints.append(fingerprint(
-                wv, document_iterator(f)))
+        with open(str(input_file)) as f:
+            fingerprints.append(fingerprint(wv, document_iterator(f)))
     fingerprints_matrix = np.stack(fingerprints)
-    print(labels)
-    print(fingerprints_matrix.shape)
+    label_list = ['A2', 'A2/B1', 'B1', 'B1/B2', 'B2', 'B2/C1', 'C1']
+    ids = list(map(label_list.index, labels))
+    print('Computing t-SNE embeddings ...')
+    embedded = TSNE(n_components=2).fit_transform(fingerprints_matrix)
+    for cefr in label_list:
+        mask = np.array(labels) == cefr
+        xs = embedded[mask, 0]
+        ys = embedded[mask, 1]
+        plt.plot(xs, ys, 'o', label=cefr)
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
