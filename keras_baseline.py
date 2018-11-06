cc613067e43ce2b16456547f8b38faad1b2346e4
Stig Johan Berggren <stigjb@student.matnat.uio.no>
2018-11-06 23:16:39 +0100

    add classification report

--- keras_baseline.py
+++ keras_baseline.py
@@ -1,12 +1,14 @@
 from itertools import chain
 from pathlib import Path
 
+import numpy as np
 from keras.models import Model
 from keras.layers import Embedding
 from keras.layers import Input, Conv1D, Dropout, Dense, GlobalMaxPooling1D, Concatenate
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
+from sklearn.metrics import classification_report, confusion_matrix
 
 from utils import load_train_and_dev, conll_reader
 
@@ -69,8 +71,16 @@ def main():
     model = build_model(vocab_size, seq_length)
     model.summary()
     model.fit(
-        train_matrix, train_y, epochs=10, batch_size=8,
+        train_matrix, train_y, epochs=20, batch_size=8,
         validation_data=(dev_matrix, dev_y), verbose=2)
+
+    predictions = model.predict(dev_matrix)
+    print(classification_report(np.argmax(dev_y, axis=1),
+                                np.argmax(predictions, axis=1),
+                                target_names=labels))
+    print("== Confusion matrix ==")
+    print(confusion_matrix(np.argmax(dev_y, axis=1),
+                           np.argmax(predictions, axis=1)))
 
 
 if __name__ == '__main__':
