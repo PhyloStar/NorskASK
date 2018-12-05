1f24277096b9ab4d7cdd8562d63c6009f0b01ceb
Stigjb <stigjb@gmail.com>
2018-12-05 13:02:41 +0100

    amend again

--- masterthesis/models/report.py
+++ masterthesis/models/report.py
@@ -15,8 +15,6 @@ def _collapse_array(arr):
 
 def report(true, pred, labels):
     print(classification_report(true, pred, target_names=labels))
-    print("Macro f1: %.3f" % f1_score(true, pred, average='macro'))
-    print("Weighted f1: %.3f" % f1_score(true, pred, average='weighted'))
     print('Accuracy: %.3f' % accuracy_score(true, pred))
     print('Macro F1: %.3f' % f1_score(true, pred, average='macro'))
     print('Micro F1: %.3f' % f1_score(true, pred, average='micro'))
@@ -29,16 +27,17 @@ def report(true, pred, labels):
         print("=== Using collapsed labels ===")
         fig, axes = plt.subplots(1, 2)
         try:
-            collapsed_true = (true + 1) // 2
-            collapsed_pred = (pred + 1) // 2
+            collapsed_true = _collapse_array(true)
+            collapsed_pred = _collapse_array(pred)
         except ValueError:
             collapsed_true = [round_cefr_score(s) for s in true]
             collapsed_pred = [round_cefr_score(s) for s in pred]
         collapsed_labels = ['A2', 'B1', 'B2', 'C1']
         print(classification_report(collapsed_true, collapsed_pred, target_names=collapsed_labels))
-        print("Macro f1: %.3f" % f1_score(collapsed_true, collapsed_pred, average='macro'))
-        print("Weighted f1: %.3f" % f1_score(collapsed_true, collapsed_pred, average='weighted'))
         print('Accuracy: %.3f' % accuracy_score(collapsed_true, collapsed_pred))
+        print("Macro f1: %.3f" % f1_score(collapsed_true, collapsed_pred, average='macro'))
+        print("Micro f1: %.3f" % f1_score(collapsed_true, collapsed_pred, average='micro'))
+        print("Weighted f1: %.3f" % f1_score(collapsed_true, collapsed_pred, average='weighted'))
         print("== Confusion matrix ==")
         collapsed_conf_matrix = confusion_matrix(collapsed_true, collapsed_pred)
         print(collapsed_conf_matrix)
