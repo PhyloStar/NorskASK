a7fc99df5d15d8b12d0a7ed7cc7e9d2f490af729
Stig Johan Berggren <stigjb@student.matnat.uio.no>
2019-01-22 13:11:55 +0100

    add test for vectorizer

--- tests/test_build_features.py
+++ tests/test_build_features.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 from unittest.mock import patch
 
+from numpy.testing import assert_equal
+
 from masterthesis.features.build_features import iterate_docs, iterate_tokens
+from masterthesis.models.taghipour_ng import preprocess
 
 test_data_dir = Path(__file__).parent / 'test_data'
+test_doc_len = 21
 
 
 class MockMeta:
@@ -26,3 +30,22 @@ def test_iterate_tokens(mock_load_split):
     tokens = list(iterate_tokens())
     assert tokens[:4] == 'Dette er første linje'.split()
     assert tokens[-4:] == 'har to setninger .'.split()
+
+
+@patch('masterthesis.features.build_features.load_split')
+@patch('masterthesis.features.build_features.data_folder', new=test_data_dir)
+def test_vectorizer(mock_load_split):
+    seq_len = test_doc_len + 1
+    w2i = {
+        "__PAD__": 0,
+        "__UNK__": 1,
+        "Dette": 2,
+        "er": 3,
+        "første": 4,
+        "linje": 5,
+    }
+    mock_load_split.return_value = MockMeta()
+    t, d = preprocess(seq_len, [0], [0], w2i)
+    assert_equal(t[0, :4], [2, 3, 4, 5])  # Dette er første linje
+    assert_equal(t[0, 4:7], [1, 1, 1])  # i dokumentet .
+    assert t[0, -1] == 0  # __PAD__
