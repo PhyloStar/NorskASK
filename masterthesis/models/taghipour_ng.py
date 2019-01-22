f4053968803e80ec34c3775181c46f84cd522216
Stig Johan Berggren <stigjb@student.matnat.uio.no>
2019-01-22 12:38:35 +0100

    add attention

--- masterthesis/models/taghipour_ng.py
+++ masterthesis/models/taghipour_ng.py
@@ -4,10 +4,14 @@ import tempfile
 from collections import Counter
 
 import numpy as np
-from keras.layers import Input, Dense, Embedding, LSTM, Dropout
+from keras.layers import (
+    Input, Dense, Embedding, LSTM, Dropout, Bidirectional, Activation,
+    TimeDistributed, Lambda, RepeatVector, Multiply, Permute, Flatten
+)
 from keras.models import Model
 from keras.optimizers import RMSprop
 from keras.utils import to_categorical
+from keras import backend as K
 
 from masterthesis.features.build_features import iterate_tokens, iterate_docs
 from masterthesis.utils import load_split, project_root
@@ -35,18 +39,41 @@ def parse_args():
     parser.add_argument('--decay-rate', type=float, default=0.9)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--dropout-rate', type=float, default=0.5)
+    parser.add_argument('--attention', action="store_true")
+    parser.add_argument('--bidirectional', action="store_true")
     return parser.parse_args()
 
 
 def build_model(vocab_size: int, sequence_len: int, num_classes: int,
-                embed_dim: int, rnn_dim: int, dropout_rate: float):
+                embed_dim: int, rnn_dim: int, dropout_rate: float,
+                bidirectional: bool, attention: bool):
+    mask_zero = not attention  # The attention mechanism does not support masked inputs
     input_ = Input((sequence_len,))
-    lookup = Embedding(vocab_size, embed_dim, mask_zero=True)(input_)
-    lstm = LSTM(rnn_dim, return_sequences=True, dropout=INPUT_DROPOUT,
-                recurrent_dropout=RECURRENT_DROPOUT)(lookup)
+    lookup = Embedding(vocab_size, embed_dim, mask_zero=mask_zero)(input_)
+
+    lstm_factory = LSTM(rnn_dim, return_sequences=True, dropout=INPUT_DROPOUT,
+                        recurrent_dropout=RECURRENT_DROPOUT)
+    if bidirectional:
+        lstm_factory = Bidirectional(lstm_factory)
+    lstm = lstm_factory(lookup)
     dropout = Dropout(dropout_rate)(lstm)
-    mean_over_time = GlobalAveragePooling1D()(dropout)
-    output = Dense(num_classes, activation='softmax')(mean_over_time)
+
+    if attention:
+        units = 2 * rnn_dim if bidirectional else rnn_dim
+        # compute importance for each step
+        attention = TimeDistributed(Dense(1, activation='tanh'))(dropout)
+        attention = Flatten()(attention)
+        attention = Activation('softmax')(attention)
+        attention = RepeatVector(units)(attention)
+        attention = Permute([2, 1])(attention)
+
+        # apply the attention
+        sent_representation = Multiply()([dropout, attention])
+        pooled = Lambda(lambda xin: K.sum(xin, axis=1))(sent_representation)
+    else:
+        pooled = GlobalAveragePooling1D()(dropout)
+
+    output = Dense(num_classes, activation='softmax')(pooled)
     return Model(inputs=[input_], outputs=[output])
 
 
@@ -94,7 +121,9 @@ def main():
     dev_y = to_categorical([labels.index(c) for c in dev_meta.cefr])
 
     model = build_model(
-        vocab_size, SEQ_LEN, len(labels), args.embed_dim, args.rnn_dim, args.dropout_rate)
+        vocab_size=vocab_size, sequence_len=SEQ_LEN, num_classes=len(labels),
+        embed_dim=args.embed_dim, rnn_dim=args.rnn_dim, dropout_rate=args.dropout_rate,
+        bidirectional=args.bidirectional, attention=args.attention)
     model.summary()
     model.compile(
         optimizer=RMSprop(lr=args.lr, rho=args.decay_rate),
