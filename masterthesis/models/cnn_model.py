4336ba040102e77f1bdc4e406e2d4ee1cd912aae
Stig Johan Berggren <stigjb@student.matnat.uio.no>
2019-02-13 15:59:14 +0100

    writing and cnning

--- masterthesis/models/cnn_model.py
+++ masterthesis/models/cnn_model.py
@@ -11,15 +11,19 @@ from keras.layers import (
 from keras.models import Model
 from keras.utils import to_categorical
 import numpy as np
+from tqdm import tqdm
 
 from masterthesis.features.build_features import (
     make_mixed_pos2i, make_pos2i, make_w2i, mixed_pos_to_sequences, pos_to_sequences,
     words_to_sequences
 )
+from masterthesis.gensim_utils import load_embeddings
 from masterthesis.models.callbacks import F1Metrics
 from masterthesis.models.report import report
 from masterthesis.results import save_results
 from masterthesis.utils import get_file_name, load_split, REPRESENTATION_LAYER, save_model
+
+EMB_LAYER_NAME = 'embedding_layer'
 
 
 def int_list(strlist: str) -> List[int]:
@@ -46,19 +50,20 @@ def parse_args():
     parser.add_argument('--batch-size', '-b', type=int)
     parser.add_argument('--doc-length', '-l', type=int)
     parser.add_argument('--vocab-size', '-s', type=int)
+    parser.add_argument('--embed-dim', type=int)
     parser.add_argument('--vectors', '-V', type=Path)
     parser.add_argument('--save-model', action='store_true')
-    parser.set_defaults(epochs=30, doc_length=700, vocab_size=4000, batch_size=32)
+    parser.set_defaults(epochs=50, doc_length=700, vocab_size=4000, batch_size=32, embed_dim=50)
     return parser.parse_args()
 
 
 def build_model(vocab_size: int, sequence_length: int, num_classes: int,
                 windows: Iterable[int], num_pos: int = 0) -> Model:
     word_input_layer = Input((sequence_length,))
-    word_embedding_layer = Embedding(vocab_size, 50)(word_input_layer)
+    word_embedding_layer = Embedding(vocab_size, 50, name=EMB_LAYER_NAME)(word_input_layer)
     if num_pos > 0:
         pos_input_layer = Input((sequence_length,))
-        pos_embedding_layer = Embedding(num_pos, 50)(pos_input_layer)
+        pos_embedding_layer = Embedding(num_pos, 10)(pos_input_layer)
         embedding_layer = Concatenate()([word_embedding_layer, pos_embedding_layer])
         inputs = [word_input_layer, pos_input_layer]
     else:
@@ -70,15 +75,13 @@ def build_model(vocab_size: int, sequence_length: int, num_classes: int,
         conv_layer = Conv1D(
             filters=100, kernel_size=kernel_size, activation='relu')(embedding_layer)
         pooled_feature_maps.extend([
-            GlobalAveragePooling1D()(conv_layer),
+            # GlobalAveragePooling1D()(conv_layer),
             GlobalMaxPooling1D()(conv_layer)
         ])
     merged = Concatenate(name=REPRESENTATION_LAYER)(pooled_feature_maps)
     dropout_layer = Dropout(0.5)(merged)
     output_layer = Dense(num_classes, activation='softmax')(dropout_layer)
-    model = Model(inputs=inputs, outputs=output_layer)
-    model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
-    return model
+    return Model(inputs=inputs, outputs=output_layer)
 
 
 def main():
@@ -113,6 +116,25 @@ def main():
     model = build_model(args.vocab_size, seq_length, len(labels),
                         windows=args.windows, num_pos=num_pos)
     model.summary()
+
+    if args.vectors:
+        if not args.vectors.is_file():
+            if 'SUBMITDIR' in os.environ:
+                args.vectors = Path(os.environ['SUBMITDIR'] / args.vectors)
+            print('New path: %r' % args.vectors)
+        if not args.vectors.is_file():
+            print('Embeddings path not available, searching for submitdir')
+        else:
+            kv = load_embeddings(args.vectors)
+            embed_dim = kv.vector_size
+            embeddings_matrix = np.zeros((args.vocab_size, embed_dim))
+            print('Making embeddings:')
+            for word, idx in tqdm(w2i.items(), total=len(w2i)):
+                vec = kv.word_vec(word)
+                embeddings_matrix[idx, :] = vec
+            model.get_layer(EMB_LAYER_NAME).set_weights([embeddings_matrix])
+
+    model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 
     temp_handle, weights_path = tempfile.mkstemp(suffix='.h5')
     callbacks = [F1Metrics(dev_x, dev_y, weights_path)]
