b8c7d03ee27bedd52e16dee64e9c009bfcac8bed
Stig Johan Berggren <stigjb@gmail.com>
2019-02-20 13:55:14 +0100

    more linear experiments

--- masterthesis/models/linear_baseline.py
+++ masterthesis/models/linear_baseline.py
@@ -1,27 +1,74 @@
 import argparse
+from typing import Iterable, Optional
 
 import sklearn.linear_model
+from sklearn.feature_extraction.text import CountVectorizer
 
-from masterthesis.features.build_features import bag_of_words, filename_iter
+from masterthesis.features.build_features import (
+    bag_of_words, filename_iter, iterate_mixed_pos_docs, iterate_pos_docs
+)
 from masterthesis.models.report import report
 from masterthesis.results import save_results
-from masterthesis.utils import load_split
+from masterthesis.utils import DATA_DIR, load_split
+
+
+conll_folder = DATA_DIR / 'conll'
+
+
+def pos_line_iter(split) -> Iterable[str]:
+    for doc in iterate_pos_docs(split):
+        yield ' '.join(doc)
+
+
+def mixed_pos_line_iter(split) -> Iterable[str]:
+    for doc in iterate_mixed_pos_docs(split):
+        yield ' '.join(doc)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument('kind', choices={'bow', 'char', 'pos', 'mixed'}, default='bow')
     parser.add_argument('--round-cefr', action='store_true')
     return parser.parse_args()
 
 
+def preprocess(kind: str, max_features: Optional[int], train_meta, dev_meta):
+    if kind == 'pos':
+        vectorizer = CountVectorizer(
+            lowercase=False, token_pattern=r"[^\s]+",
+            ngram_range=(2, 4), max_features=max_features)
+        train_x = vectorizer.fit_transform(pos_line_iter('train'))
+        dev_x = vectorizer.transform(pos_line_iter('dev'))
+        num_features = len(vectorizer.vocabulary_)
+    elif kind == 'mixed':
+        vectorizer = CountVectorizer(
+            lowercase=False, token_pattern=r"[^\s]+",
+            ngram_range=(1, 3), max_features=max_features)
+        train_x = vectorizer.fit_transform(mixed_pos_line_iter('train'))
+        dev_x = vectorizer.transform(mixed_pos_line_iter('dev'))
+        num_features = len(vectorizer.vocabulary_)
+    elif kind == 'char':
+        train_x, vectorizer = bag_of_words(
+            'train', analyzer='char', ngram_range=(1, 3), max_features=max_features)
+        dev_x = vectorizer.transform(filename_iter(dev_meta))
+        num_features = len(vectorizer.vocabulary_)
+    elif kind == 'bow':
+        train_x, vectorizer = bag_of_words('train', max_features=max_features)
+        dev_x = vectorizer.transform(filename_iter(dev_meta))
+        num_features = len(vectorizer.vocabulary_)
+    else:
+        raise ValueError('Feature type "%s" is not supported' % kind)
+    print("Number of features is %d" % len(vectorizer.vocabulary_))
+    return train_x, dev_x, num_features
+
+
 def main():
     args = parse_args()
-    train_x, vectorizer = bag_of_words('train')
-    print("Number of features is %d" % len(vectorizer.vocabulary_))
-    dev_meta = load_split('dev', round_cefr=args.round_cefr)
-    dev_x = vectorizer.transform(filename_iter(dev_meta))
-
     train_meta = load_split('train', round_cefr=args.round_cefr)
+    dev_meta = load_split('dev', round_cefr=args.round_cefr)
+
+    train_x, dev_x, num_features = preprocess(args.kind, None, train_meta, dev_meta)
+
     labels = sorted(train_meta.cefr.unique())
 
     train_y = [labels.index(c) for c in train_meta.cefr]
--- masterthesis/models/mlp_baseline.py
+++ masterthesis/models/mlp_baseline.py
@@ -10,11 +10,13 @@ from keras.utils import to_categorical
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
-from masterthesis.features.build_features import bag_of_words, filename_iter
+from masterthesis.features.build_features import (
+    bag_of_words, filename_iter, iterate_mixed_pos_docs, iterate_pos_docs
+)
 from masterthesis.models.callbacks import F1Metrics
 from masterthesis.models.report import report
 from masterthesis.results import save_results
-from masterthesis.utils import conll_reader, DATA_DIR, load_split, REPRESENTATION_LAYER
+from masterthesis.utils import DATA_DIR, load_split, REPRESENTATION_LAYER
 
 
 conll_folder = DATA_DIR / 'conll'
@@ -22,7 +24,7 @@ conll_folder = DATA_DIR / 'conll'
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('featuretype', choices=['pos', 'word', 'char'])
+    parser.add_argument('featuretype', choices={'pos', 'word', 'char', 'mixed'})
     parser.add_argument('--round-cefr', action='store_true')
     parser.add_argument('--max-features', type=int, default=10000)
     parser.add_argument('--lr', type=float, default=2e-4)
@@ -39,13 +41,14 @@ def build_model(vocab_size: int, num_classes: int):
     return Model(inputs=[input_], outputs=[output])
 
 
-def pos_line_iter(meta) -> Iterable[str]:
-    for stem in meta.filename:
-        file = (conll_folder / stem).with_suffix('.conll')
-        sents = []
-        for sent in conll_reader(file, ['UPOS'], tags=False):
-            sents.append(' '.join(tag for (tag,) in sent))
-        yield '\n'.join(sents)
+def pos_line_iter(split) -> Iterable[str]:
+    for doc in iterate_pos_docs(split):
+        yield ' '.join(doc)
+
+
+def mixed_pos_line_iter(split) -> Iterable[str]:
+    for doc in iterate_mixed_pos_docs(split):
+        yield ' '.join(doc)
 
 
 def preprocess(kind: str, max_features: int, train_meta, dev_meta):
@@ -53,8 +56,15 @@ def preprocess(kind: str, max_features: int, train_meta, dev_meta):
         vectorizer = CountVectorizer(
             lowercase=False, token_pattern=r"[^\s]+",
             ngram_range=(2, 4), max_features=max_features)
-        train_x = vectorizer.fit_transform(pos_line_iter(train_meta))
-        dev_x = vectorizer.transform(pos_line_iter(dev_meta))
+        train_x = vectorizer.fit_transform(pos_line_iter('train'))
+        dev_x = vectorizer.transform(pos_line_iter('dev'))
+        num_features = len(vectorizer.vocabulary_)
+    elif kind == 'mixed':
+        vectorizer = CountVectorizer(
+            lowercase=False, token_pattern=r"[^\s]+",
+            ngram_range=(1, 3), max_features=max_features)
+        train_x = vectorizer.fit_transform(mixed_pos_line_iter('train'))
+        dev_x = vectorizer.transform(mixed_pos_line_iter('dev'))
         num_features = len(vectorizer.vocabulary_)
     elif kind == 'char':
         train_x, vectorizer = bag_of_words(
